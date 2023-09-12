97f8eca0d4204930c2b68c5e1f32da0a05d199ba
Abderaim Elouardy <elouardy.abderrahim07@gmail.com>
2023-09-12 21:17:56 +0100

    feat: user entity

---
+++ src/main/java/com/libray/entity/user.java
@@ -0,0 +1,40 @@
+package com.libray.entity;
+
+public class user {
+    private String lastName,firstName,email,password;
+
+    public user() {
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
