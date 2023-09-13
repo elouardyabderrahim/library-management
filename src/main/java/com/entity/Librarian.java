807a060e95903368dc8433eb44c2e87f8f0dd8b3
Abderaim Elouardy <elouardy.abderrahim07@gmail.com>
2023-09-13 11:53:15 +0100

    feat: add the employeeId for librarian and cardNumber for reader

--- src/main/java/com/entity/Librarian.java
+++ src/main/java/com/entity/Librarian.java
@@ -1,8 +1,17 @@
 package com.entity;
 
 public class Librarian extends User {
-
-    public Librarian(long userId,String lastName, String  firstName,String email,String password) {
+ private String employeeID;
+    public Librarian(long userId,String employeeID,String lastName, String  firstName,String email,String password) {
     super(userId,lastName,firstName,email,password);
+    this.employeeID=employeeID;
+    }
+
+    public String getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(String employeeID) {
+        this.employeeID = employeeID;
     }
 }
--- src/main/java/com/entity/Reader.java
+++ src/main/java/com/entity/Reader.java
@@ -1,13 +1,20 @@
 package com.entity;
 
 public class Reader extends User {
-
+    private String cardNumber;
     public Reader() {
     }
 
-    public Reader(long userId,String lastName, String  firstName,String email,String password) {
+    public Reader(long userId,String cardNumber,String lastName, String  firstName,String email,String password) {
         super(userId,lastName,firstName,email,password);
+        this.cardNumber=cardNumber;
     }
 
+    public String getCardNumber() {
+        return cardNumber;
+    }
 
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
 }
